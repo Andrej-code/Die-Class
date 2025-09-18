e18340531c6bdb3abf263a75463b09b7b8c26404
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-09-18 09:39:56 -0400

    Add project files.

---
+++ Die Class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime;
+
+namespace Die_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1, die2;
+            int sum;
+
+            die1 = new Die();
+            die2 = new Die(ConsoleColor.DarkRed);
+
+            Console.WriteLine("Die 1 is a " + die1);
+            die1.DrawRoll();
+
+            Console.WriteLine("Die 2 is a " + die2);
+            die2.DrawRoll();
+
+            sum = 7;
+
+            if (die1.Roll == die2.Roll)
+            {
+                Console.WriteLine("You rolled Doubles.");
+
+            }
+            if (die1.Roll + die2.Roll == sum)
+            {
+                Console.WriteLine("You rolled a Sum of 7.");
+            }
+            if (die1.Roll == die2.Roll)
+            {
+                Console.WriteLine("You rolled Snake Eyes.");
+            }
+            //if ()
+            //{
+            //    Console.WriteLine("You rooled a Even sum.");
+            //}
+
+        }
+    }
+}
